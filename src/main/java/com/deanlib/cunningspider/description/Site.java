7dd86915122e63eba85ff506d84c2e12786969e8
Dean <abcruoshui@sina.com>
2019-01-28 16:50:17 +0800

    update

--- src/main/java/com/deanlib/cunningspider/description/Site.java
+++ src/main/java/com/deanlib/cunningspider/description/Site.java
@@ -5,14 +5,19 @@ import java.util.List;
 /**
  * 定义网站的结构，不变
  * 由四部分组成
- * catalog - list - detail - related
+ * catalog - list - resource - related
+ * 虽然以上是一个完整的结构，但不需要搞一个完整结构
+ * 采用 list - resource - related 的方式，可以随着网站的更新，而自动更新
+ * list 描述首页
  */
 public abstract class Site extends Description {
 
+
     Page catalog;
     Page list;
-    Page detail;
+    Page resource;
     Page related;
+
 
     public Page getCatalog() {
         return catalog;
@@ -30,12 +35,12 @@ public abstract class Site extends Description {
         this.list = list;
     }
 
-    public Page getDetail() {
-        return detail;
+    public Page getResource() {
+        return resource;
     }
 
-    public void setDetail(Page detail) {
-        this.detail = detail;
+    public void setResource(Page resource) {
+        this.resource = resource;
     }
 
     public Page getRelated() {
--- src/main/java/com/deanlib/cunningspider/test/description/MM131.java
+++ src/main/java/com/deanlib/cunningspider/test/description/MM131.java
@@ -4,13 +4,13 @@ import com.deanlib.cunningspider.description.*;
 
 public class MM131 extends Site {
     public MM131() {
-        setCatalog(new Page("http://www.mm131.com/",new Action(
-                new Key(new KeyElement(KeyElement.FIND_TYPE_CLASS,"nav",null,
-                new KeyElement(KeyElement.FIND_TYPE_TAG,"a",null,
-                        KeyElement.RESULT_TYPE_TEXT,null)),
-                new KeyElement(KeyElement.FIND_TYPE_CLASS,"nav",null,
-                new KeyElement(KeyElement.FIND_TYPE_TAG,"a",null,
-                        KeyElement.RESULT_TYPE_ATTR,"href"))),1,0)));
+//        setCatalog(new Page("http://www.mm131.com/",new Action(
+//                new Key(new KeyElement(KeyElement.FIND_TYPE_CLASS,"nav",null,
+//                new KeyElement(KeyElement.FIND_TYPE_TAG,"a",null,
+//                        KeyElement.RESULT_TYPE_TEXT,null)),
+//                new KeyElement(KeyElement.FIND_TYPE_CLASS,"nav",null,
+//                new KeyElement(KeyElement.FIND_TYPE_TAG,"a",null,
+//                        KeyElement.RESULT_TYPE_ATTR,"href"))),1,0)));
         setList(new Page(new Action(new Key(new KeyElement(KeyElement.FIND_TYPE_CLASS,"list-left public-box",null,
                 new KeyElement(KeyElement.FIND_TYPE_ATTRIBUTE,"_blank","target",
                         new KeyElement(KeyElement.FIND_TYPE_TAG,"img",null,
@@ -19,7 +19,7 @@ public class MM131 extends Site {
                 "list-left public-box",null,
                 new KeyElement(KeyElement.FIND_TYPE_ATTRIBUTE,"_blank",
                         "target",KeyElement.RESULT_TYPE_ATTR,"href"))))));
-        setDetail(new Page(new Action(new Key(new KeyElement(KeyElement.FIND_TYPE_CLASS,"content-pic",null,
+        setResource(new Page(new Action(new Key(new KeyElement(KeyElement.FIND_TYPE_CLASS,"content-pic",null,
                 new KeyElement(KeyElement.FIND_TYPE_TAG,"img",null,
                         KeyElement.RESULT_TYPE_ATTR,"src"))))));
 
