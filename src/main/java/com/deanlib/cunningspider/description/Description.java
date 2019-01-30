f3d5387c336f18c052ab0ee3f52cf531f5637fcb
Dean King <abcruoshui@sina.com>
2019-01-30 22:56:56 +0800

    fix 2

--- src/main/java/com/deanlib/cunningspider/description/Description.java
+++ src/main/java/com/deanlib/cunningspider/description/Description.java
@@ -1,6 +1,8 @@
 package com.deanlib.cunningspider.description;
 
-public class Description {
+import java.io.Serializable;
+
+public class Description implements Serializable {
 
     String name;
     int version;
