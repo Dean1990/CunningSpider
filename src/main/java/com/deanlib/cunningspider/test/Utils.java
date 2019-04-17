3b0013fd59925402de3852dfe900125a6b0ccb52
Dean King <abcruoshui@sina.com>
2019-04-18 00:33:13 +0800

    fix

--- src/main/java/com/deanlib/cunningspider/test/Utils.java
+++ src/main/java/com/deanlib/cunningspider/test/Utils.java
@@ -10,27 +10,17 @@ public class Utils {
         String reqHeader = "Accept\t\n" +
                 "image/webp,*/*\n" +
                 "Accept-Encoding\t\n" +
-                "gzip, deflate, br\n" +
+                "gzip, deflate\n" +
                 "Accept-Language\t\n" +
                 "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2\n" +
-                "Cache-Control\t\n" +
-                "max-age=0\n" +
                 "Connection\t\n" +
                 "keep-alive\n" +
-                "Cookie\t\n" +
-                "Hm_lvt_c87959b92e43d58c8a26e9b…8c8a26e9b70e5a6795=1553161397\n" +
                 "Host\t\n" +
-                "pic.qqtn.com\n" +
-                "If-Modified-Since\t\n" +
-                "Tue, 20 Mar 2018 08:37:07 GMT\n" +
-                "If-None-Match\t\n" +
-                "\"EE6B17A99F92D62BB7F57F2400BA9C21\"\n" +
+                "i1.whymtj.com\n" +
                 "Referer\t\n" +
-                "https://www.qqtn.com/article/article_121388_1.html\n" +
-                "TE\t\n" +
-                "Trailers\n" +
+                "http://www.umei.cc/p/gaoqing/xiuren_VIP/180253.htm\n" +
                 "User-Agent\t\n" +
-                "Mozilla/5.0 (Macintosh; Intel …) Gecko/20100101 Firefox/65.0";
+                "Mozilla/5.0 (Windows NT 10.0; …) Gecko/20100101 Firefox/66.0";
         String code = createCode_addCoverHeader(requestHeaderTransform(reqHeader));
         System.out.println(code);
     }
